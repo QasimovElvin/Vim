a00aceb97e48aa93c2d7d2ede81073ec26261d68
QasimovElvin <qasimove585@gmail.com>
2023-02-23 23:33:32 +0400

    Craeated

---
+++ main.cs
@@ -0,0 +1,7 @@
+int a = 15;
+
+string name = "Elvin";
+
+bool  flag = false;
+
+char Ravi = 'B';
---
+++ main.js
@@ -0,0 +1,10 @@
+var number = 2
+
+char  b = '5' 
+
+boolean flag  = true
+
+
+var name = "Elvin"
+
+string surename = "Qasimov"
